34ec5f775a9c063728d63ec269403071678b9a6b
gotpl <631707329@qq.com>
2021-01-14 18:09:21 +0800

    update

--- utils/json_draw.py
+++ utils/json_draw.py
@@ -1,4 +1,5 @@
 import json
+import math
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,12 +8,15 @@ import matplotlib.font_manager as fm
 json_list = sys.argv[1:]
 length = None #None
 show_all = True
+scale = 20
 acc_loss = []
 acc_grad = []
+xticks = []
 with open(json_list[0], 'r') as f:
     metrics_dic = json.load(f)
     train_accuracies = metrics_dic['train_accuracies']
-    for acc in train_accuracies:
+    for i, acc in enumerate(train_accuracies):
+        xticks.append(i)
         if show_all:
             acc_loss.extend([a/b for a,b in zip(acc[3], acc[2])])
         else:
@@ -28,11 +32,15 @@ with open(json_list[1], 'r') as f:
 
 
 sub_length = length if length is not None else len(acc_loss)
-X = [ i for i in range(sub_length)]
-acc_loss = acc_loss[:sub_length]
-acc_grad = acc_grad[:sub_length]
-ln1, = plt.plot(X,acc_loss,color='red',linewidth=1.0,linestyle='-')
-ln2, = plt.plot(X,acc_grad,color='blue',linewidth=1.0,linestyle='-')
+acc_loss = acc_loss[:sub_length:scale]
+acc_grad = acc_grad[:sub_length:scale]
+X = [ i for i in range(0,len(acc_grad))]
+
+ln1, = plt.plot(X,acc_loss,color='red',linewidth=0.3,linestyle='-')
+ln2, = plt.plot(X,acc_grad,color='blue',linewidth=0.3,linestyle='-')
+xticks = xticks[::20]
+a = math.ceil(len(X)/len(xticks))
+plt.xticks(X[::a], xticks)
 
 my_font = fm.FontProperties(fname="../font/wqy-microhei.ttc")
 plt.title("训练精度比较",fontproperties=my_font) #设置标题及字体
